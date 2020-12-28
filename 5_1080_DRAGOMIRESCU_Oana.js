c28a979cd1ec62f03b94b907f100c1667a00c988
Oana Dragomirescu <wannawdo@gmail.com>
2020-12-29 00:02:45 +0200

    coliziuni nava asteroizi

--- 5_1080_DRAGOMIRESCU_Oana.js
+++ 5_1080_DRAGOMIRESCU_Oana.js
@@ -10,24 +10,27 @@ const acceleratieNava = 2; //pixeli pe secunde
 const FPS = 30; //frames per second
 const vitezaIntoarcere = 360;//in grade pe secunda
 const fortaFrecare = 0.7; //coeficientul de frecare al spatiului 0=nu avem forta de frecare
-const durataExplozieNava=0.5;//cat dureaza explozia navei
-const imunitateNava=5;//cat timp nava nu poate fi omorata de asteroizi cand se reseteaza
-const durataLicaritNava=0.2;//cat licareste nava cat e imuna
+const durataExplozieNava = 0.5;//cat dureaza explozia navei
+const imunitateNava = 2;//cat timp nava nu poate fi omorata de asteroizi cand se reseteaza
+const durataLicaritNava = 0.1;//cat licareste nava cat e imuna
+const nrMaximRachete = 2;//nr maxim de rachete care pot fi lansate  simultan pe ecran
+const vitezaRachete = 450;//in pixeli pe secunde
+const distantaRachete = 0.4;//1=canvas.width;
 
 var nava = navaNoua();//{
-    // x: canvas.width / 2,
-    // y: canvas.height / 2,
-    // //sa apara la mijloc
-    // r: dimensiuneNava / 2,
-    // a: 90 / 180 * Math.PI, //conversie in radius; angle - directia navei
-    // rotire: 0,
-    // timpExplozie:0,
-    // //pentru mers:
-    // inainteaza: false,
-    // inaintare: {
-    //     x: 0,
-    //     y: 0
-    // }
+// x: canvas.width / 2,
+// y: canvas.height / 2,
+// //sa apara la mijloc
+// r: dimensiuneNava / 2,
+// a: 90 / 180 * Math.PI, //conversie in radius; angle - directia navei
+// rotire: 0,
+// timpExplozie:0,
+// //pentru mers:
+// inainteaza: false,
+// inaintare: {
+//     x: 0,
+//     y: 0
+// }
 
 //}
 
@@ -41,7 +44,20 @@ var dimensiuneAsteroizi = 120;//dimensiunea initiala a asteroizilor
 var varfuriAsteroizi = 12;//nr mediu de varfuri ale asteroizilor
 
 
-creeazaAsteroizi();
+
+
+function asteroidNou(x, y, r) {
+    var asteroid = {
+        x: x,
+        y: y,
+        xv: Math.random() * vitezaAsteriozi / FPS * (Math.random() < 0.5 ? 1 : -1), //viteza aseroid pe ox
+        yv: Math.random() * vitezaAsteriozi / FPS * (Math.random() < 0.5 ? 1 : -1), //viteza aseroid pe oy
+        r: r,
+        a: Math.random() * 2 * Math.PI, //in radiani
+        varf: Math.floor(Math.random() * (varfuriAsteroizi + 1) + varfuriAsteroizi / 2) //+1 ca sa nu fie 0
+    };
+    return asteroid;
+}
 
 
 function creeazaAsteroizi() {
@@ -55,7 +71,7 @@ function creeazaAsteroizi() {
         }
         while (distantaDintrePuncte(nava.x, nava.y, x, y) < dimensiuneAsteroizi * 2 + nava.r);
 
-        asteroizi.push(asteroidNou(x, y));
+        asteroizi.push(asteroidNou(x, y, Math.ceil(dimensiuneAsteroizi / 2)));
     }
 
 }
@@ -64,19 +80,28 @@ function distantaDintrePuncte(xn, yn, xa, ya) {
     return Math.sqrt(Math.pow(xa - xn, 2) + Math.pow(ya - yn, 2));
 }
 
-function asteroidNou(x, y) {
-    var asteroid = {
-        x: x,
-        y: y,
-        xv: Math.random() * vitezaAsteriozi / FPS * (Math.random() < 0.5 ? 1 : -1), //viteza aseroid pe ox
-        yv: Math.random() * vitezaAsteriozi / FPS * (Math.random() < 0.5 ? 1 : -1), //viteza aseroid pe oy
-        r: dimensiuneAsteroizi / 2,
-        a: Math.random() * 2 * Math.PI, //in radiani
-        varf: Math.floor(Math.random() * (varfuriAsteroizi + 1) + varfuriAsteroizi / 2) //+1 ca sa nu fie 0
-    };
-    return asteroid;
+function distrugeAsteroid(index) {
+    console.log(index)
+    var x = asteroizi[index].x;
+    var y = asteroizi[index].y;
+    var r = asteroizi[index].r;
+
+    //impartim asteroidul in 2
+    if (r == Math.ceil(dimensiuneAsteroizi / 2)) {
+        asteroizi.push(asteroidNou(x, y, Math.ceil(dimensiuneAsteroizi / 4)));
+        asteroizi.push(asteroidNou(x, y, Math.ceil(dimensiuneAsteroizi / 4)));
+    }
+    else
+        if (r == Math.ceil(dimensiuneAsteroizi / 4)) {
+            asteroizi.push(asteroidNou(x, y, Math.ceil(dimensiuneAsteroizi / 8)));
+            asteroizi.push(asteroidNou(x, y, Math.ceil(dimensiuneAsteroizi / 8)));
+        }
+
+    //distrug asteroidul
+    asteroizi.splice(index, 1);
 }
 
+creeazaAsteroizi();
 
 // ---------------------------------------------------------------------- ASTEROID
 
@@ -87,23 +112,25 @@ const margini = false; //margini pentru coliziune folosind cerc
 
 // ---------------------------------------------------------------------- NAVA
 
-function navaNoua(){
+function navaNoua() {
     return {
-         x: canvas.width / 2,
-         y: canvas.height / 2,
-         //sa apara la mijloc
-         r: dimensiuneNava / 2,
-         a: 90 / 180 * Math.PI, //conversie in radius; angle - directia navei
-         rotire: 0,
-         timpExplozie:0,
-         timpLicarit:Math.ceil(durataLicaritNava*FPS),
-         nrLicarit:Math.ceil(imunitateNava/durataLicaritNava),
-         //pentru mers:
-         inainteaza: false,
-         inaintare: {
-             x: 0,
-             y: 0
-         }
+        x: canvas.width / 2,
+        y: canvas.height / 2,
+        //sa apara la mijloc
+        r: dimensiuneNava / 2,
+        a: 90 / 180 * Math.PI, //conversie in radius; angle - directia navei
+        rotire: 0,
+        timpExplozie: 0,
+        timpLicarit: Math.ceil(durataLicaritNava * FPS),
+        nrLicarit: Math.ceil(imunitateNava / durataLicaritNava),
+        //pentru mers:
+        inainteaza: false,
+        inaintare: {
+            x: 0,
+            y: 0
+        },
+        poateDeclansaRachete: true,
+        rachete: [],
     }
 }
 const keyDown = (eveniment) => {
@@ -134,6 +161,11 @@ const keyDown = (eveniment) => {
         case 39:
             nava.rotire = -vitezaIntoarcere / 180 * Math.PI / FPS;
             break;
+
+        //pentru rachete: (x)
+        case 88:
+            aruncaRachete();
+            break;
     }
 }
 const keyUp = (eveniment) => {
@@ -163,7 +195,27 @@ const keyUp = (eveniment) => {
         case 39:
             nava.rotire = 0;
             break;
+
+        //pentru rachete: (x)
+        case 88:
+            nava.poateDeclansaRachete = true;
+            break;
     }
+}
+
+function aruncaRachete() {
+    //
+    if (nava.poateDeclansaRachete && nava.rachete.length <= nrMaximRachete) {
+        nava.rachete.push({
+            //de la varful navei
+            x: nava.x + 4 / 3 * nava.r * Math.cos(nava.a),
+            y: nava.y - 4 / 3 * nava.r * Math.sin(nava.a),
+            xv: vitezaRachete * Math.cos(nava.a) / FPS,
+            yv: -vitezaRachete * Math.sin(nava.a) / FPS,
+            distRachete: 0
+        })
+    }
+    nava.poateDeclansaRachete = false;
 }
 //event handlers ca sa rotesc nava
 document.addEventListener("keydown", keyDown);
@@ -173,30 +225,30 @@ document.addEventListener("keyup", keyUp);
 setInterval(update, 1000 / FPS);
 
 
-function explodeazaNava(){
+function explodeazaNava() {
     // ctx.fillStyle="#B20000";
     // ctx.strokeStyle="#B20000";
     // ctx.beginPath();
     // ctx.arc(nava.x,nava.y,nava.r,0,2*Math.PI,false);
     // ctx.fill();
     // ctx.stroke();
-    nava.timpExplozie=Math.ceil(durataExplozieNava*FPS)
+    nava.timpExplozie = Math.ceil(durataExplozieNava * FPS);
 }
 
 
 function update() {
-     // ---------------------------------------------------------------------- EXPLOZIE
-    var explozieNava=nava.timpExplozie>0;
-    var licarit=nava.nrLicarit%2==0;
-     // ---------------------------------------------------------------------- EXPLOZIE
+    // ---------------------------------------------------------------------- EXPLOZIE
+    var explozieNava = nava.timpExplozie > 0;
+    var licarit = nava.nrLicarit % 2 == 0;
+    // ---------------------------------------------------------------------- EXPLOZIE
 
     //desenez fundalul(spatiul)
     ctx.fillStyle = "#071425";
     ctx.fillRect(0, 0, canvas.width, canvas.height); //700,700
 
     // ---------------------------------------------------------------------- NAVA
-    if(!explozieNava){
-        if(licarit){
+    if (!explozieNava) {
+        if (licarit) {
             //desenez nava in forma de triunghi
             ctx.strokeStyle = "yellow";
             ctx.lineWidth = dimensiuneNava / 20;
@@ -214,49 +266,49 @@ function update() {
 
             ctx.fillStyle = "yellow";
             ctx.fillRect(nava.x - 1, nava.y - 1, 2, 2); //cercul galben din interiorul navei
-            }
-            //ma ocup de licarit
-            if(nava.nrLicarit>0){
-                //scad trimpul de licarit
-                nava.timpLicarit--;
+        }
+        //ma ocup de licarit
+        if (nava.nrLicarit > 0) {
+            //scad trimpul de licarit
+            nava.timpLicarit--;
 
-                // scad nr de licariri
-                if(nava.timpLicarit==0){
-                    nava.timpLicarit=Math.ceil(durataLicaritNava*FPS);
-                    nava.nrLicarit--;
-                }
+            // scad nr de licariri
+            if (nava.timpLicarit == 0) {
+                nava.timpLicarit = Math.ceil(durataLicaritNava * FPS);
+                nava.nrLicarit--;
             }
         }
-        else{
-            //desenez explozia
+    }
+    else {
+        //desenez explozia
 
-            ctx.fillStyle="#7F0000";
-            ctx.beginPath();
-            ctx.arc(nava.x,nava.y,nava.r*1.9,0,2*Math.PI,false);
-            ctx.fill(); 
-            
-            ctx.fillStyle="#FF0000";
-            ctx.beginPath();
-            ctx.arc(nava.x,nava.y,nava.r*1.4,0,2*Math.PI,false);
-            ctx.fill();
+        ctx.fillStyle = "#7F0000";
+        ctx.beginPath();
+        ctx.arc(nava.x, nava.y, nava.r * 1.9, 0, 2 * Math.PI, false);
+        ctx.fill();
 
-            ctx.fillStyle="#f2af0f";
-            ctx.beginPath();
-            ctx.arc(nava.x,nava.y,nava.r*1.1,0,2*Math.PI,false);
-            ctx.fill();
+        ctx.fillStyle = "#FF0000";
+        ctx.beginPath();
+        ctx.arc(nava.x, nava.y, nava.r * 1.4, 0, 2 * Math.PI, false);
+        ctx.fill();
 
-            ctx.fillStyle="#f5f63d";
-            ctx.beginPath();
-            ctx.arc(nava.x,nava.y,nava.r*0.8,0,2*Math.PI,false);
-            ctx.fill();
+        ctx.fillStyle = "#f2af0f";
+        ctx.beginPath();
+        ctx.arc(nava.x, nava.y, nava.r * 1.1, 0, 2 * Math.PI, false);
+        ctx.fill();
 
-            ctx.fillStyle="#fdfdfd"; 
-            ctx.beginPath();
-            ctx.arc(nava.x,nava.y,nava.r*0.4,0,2*Math.PI,false);
-            ctx.fill();
+        ctx.fillStyle = "#f5f63d";
+        ctx.beginPath();
+        ctx.arc(nava.x, nava.y, nava.r * 0.8, 0, 2 * Math.PI, false);
+        ctx.fill();
 
-            
-        }
+        ctx.fillStyle = "#fdfdfd";
+        ctx.beginPath();
+        ctx.arc(nava.x, nava.y, nava.r * 0.4, 0, 2 * Math.PI, false);
+        ctx.fill();
+
+
+    }
 
     // ---------------------------------------------------------------------- COLIZIUNI
     if (margini) {
@@ -267,77 +319,147 @@ function update() {
     }
 
     // ---------------------------------------------------------------------- ACOLIZIUNI
-        
-    
-    
-   
+    // ---------------------------------------------------------------------- NAVA - RACHETE    
+    //desenez rachetele
+    for (var i = 0; i < nava.rachete.length; i++) {
+
+        ctx.fillStyle = "#FF0000";
+        ctx.beginPath();
+        ctx.arc(nava.rachete[i].x, nava.rachete[i].y, dimensiuneNava / 15, 0, 2 * Math.PI, false);
+        //x,             y,                   raza,            ungi inceput, unghi final;  
+        ctx.fill();
+
+        ctx.fillStyle = "#f5f63d";
+        ctx.beginPath();
+        ctx.arc(nava.rachete[i].x, nava.rachete[i].y, dimensiuneNava / 30, 0, 2 * Math.PI, false);
+        ctx.fill();
+    }
+
+    //cand rachetele ating un asteroid:
+    var xAsteroid;
+    var yAsteroid;
+    var rAsteroid;
+    var xRacheta;
+    var yRacheta;
+    for (var i = asteroizi.length - 1; i >= 0; i--) {
+        xAsteroid = asteroizi[i].x;
+        yAsteroid = asteroizi[i].y;
+        rAsteroid = asteroizi[i].r;
+
+        for (var j = nava.rachete.length - 1; j >= 0; j--) {
+            xRacheta = nava.rachete[j].x;
+            yRacheta = nava.rachete[j].y;
+
+            //verificam daca atinge asteroizii
+            if (distantaDintrePuncte(xAsteroid, yAsteroid, xRacheta, yRacheta) < rAsteroid) {
+                //stergem racheta
+               // nava.rachete.splice(j, i);
+
+                //stergem astertoidul
+                distrugeAsteroid(i);
+
+                break;
+            }
+        }
+
+    }
+    // ---------------------------------------------------------------------- NAVA - RACHETE
+    // ---------------------------------------------------------------------- NAVA 
     //mut nava
     if (nava.inainteaza) {
         nava.inaintare.x = nava.inaintare.x + 0.8 * acceleratieNava * Math.cos(nava.a) / FPS;
         nava.inaintare.y = nava.inaintare.y - 0.8 * acceleratieNava * Math.sin(nava.a) / FPS;
 
-        if(!explozieNava && licarit){
-        //ii desenez "focul"
-        ctx.fillStype = "yellow"
-        ctx.strokeStyle = dimensiuneNava / 10;
-        ctx.strokeStyle = "red";
-        //incep sa desenez triunghiul
-        ctx.beginPath();
-        //spate stanga
-        ctx.moveTo(
-            nava.x - nava.r * (2 / 3 * Math.cos(nava.a) - 0.5 * Math.sin(nava.a)),
-            nava.y + nava.r * (2 / 3 * Math.sin(nava.a) + 0.5 * Math.cos(nava.a)));
-        //centru(in spatele navei)
-        ctx.lineTo(
-            nava.x - nava.r * 5 / 3 * Math.cos(nava.a),
-            nava.y + nava.r * 5 / 3 * Math.sin(nava.a)); //partea din spate din dreapta
-        ctx.lineTo(
-            nava.x - nava.r * (2 / 3 * Math.cos(nava.a) + 0.5 * Math.sin(nava.a)),
-            nava.y + nava.r * (2 / 3 * Math.sin(nava.a) - 0.5 * Math.cos(nava.a))); //partea din spate din stanga
-        ctx.closePath();
-        ctx.fill();
-        ctx.stroke();
+        if (!explozieNava && licarit) {
+            //ii desenez "focul"
+            ctx.fillStype = "yellow"
+            ctx.strokeStyle = dimensiuneNava / 10;
+            ctx.strokeStyle = "red";
+            //incep sa desenez triunghiul
+            ctx.beginPath();
+            //spate stanga
+            ctx.moveTo(
+                nava.x - nava.r * (2 / 3 * Math.cos(nava.a) - 0.5 * Math.sin(nava.a)),
+                nava.y + nava.r * (2 / 3 * Math.sin(nava.a) + 0.5 * Math.cos(nava.a)));
+            //centru(in spatele navei)
+            ctx.lineTo(
+                nava.x - nava.r * 5 / 3 * Math.cos(nava.a),
+                nava.y + nava.r * 5 / 3 * Math.sin(nava.a)); //partea din spate din dreapta
+            ctx.lineTo(
+                nava.x - nava.r * (2 / 3 * Math.cos(nava.a) + 0.5 * Math.sin(nava.a)),
+                nava.y + nava.r * (2 / 3 * Math.sin(nava.a) - 0.5 * Math.cos(nava.a))); //partea din spate din stanga
+            ctx.closePath();
+            ctx.fill();
+            ctx.stroke();
         }
-      
+
     }
     else {
         nava.inaintare.x = nava.inaintare.x - fortaFrecare * nava.inaintare.x / FPS;
         nava.inaintare.y = nava.inaintare.y - fortaFrecare * nava.inaintare.y / FPS;
     }
-   
+    // ---------------------------------------------------------------------- NAVA - RACHETE
+    //mut rachetele
+    for (var i = nava.rachete.length - 1; i >= 0; i--) {
+        //verific distanta parcursa de rachete
+        if (nava.rachete[i].distRachete > distantaRachete * canvas.width) {
+            //le ffacem sa dispara(stergem)
+            nava.rachete.splice(i, 1); //ne va schimba marimea vectorului asa ca for-ul trebuie parcurs invers
+            continue; //pentru a nu se opri la randul anterior si a merge mai departe in for
+        }
 
+        //mut rachetele
+        nava.rachete[i].x = nava.rachete[i].x + nava.rachete[i].xv;
+        nava.rachete[i].y = nava.rachete[i].y + nava.rachete[i].yv;
 
+        //calculez distanta parcursa de rachete
+        nava.rachete[i].distRachete = nava.rachete[i].distRachete + Math.sqrt(Math.pow(nava.rachete[i].xv, 2) + Math.pow(nava.rachete[i].yv, 2));
+
+        //penru marginea canvasului
+        if (nava.rachete[i].x < 0)
+            nava.rachete[i].x = canvas.width;
+        else
+            if (nava.rachete[i].x > canvas.width)
+                nava.rachete[i].x = 0;
+
+        if (nava.rachete[i].y < 0)
+            nava.rachete[i].y = canvas.height;
+        else
+            if (nava.rachete[i].y > canvas.height)
+                nava.rachete[i].y = 0;
+
+    }
+    // ---------------------------------------------------------------------- NAVA - RACHETE
     // ---------------------------------------------------------------------- NAVA
-    if(!explozieNava){
+    if (!explozieNava) {
         //mut nava 
         nava.x = nava.x + nava.inaintare.x;
         nava.y = nava.y + nava.inaintare.y;
         //rotesc nava
         nava.a += nava.rotire;
     }
-    else
-    {
+    else {
         nava.timpExplozie--;
-        if(nava.timpExplozie==0){
-            nava=navaNoua();
+        if (nava.timpExplozie == 0) {
+            nava = navaNoua();
         }
     }
 
-        //pentru a nu iesi din ecran:
-        if (nava.x < 0 - nava.r)
-            nava.x = canvas.width + nava.r;
-        else
-            if (nava.x > canvas.width + nava.r)
-                nava.x = 0 - nava.r;
-        if (nava.y < 0 - nava.r)
-            nava.y = canvas.height + nava.r;
-        else
-            if (nava.y > canvas.height + nava.r)
-                nava.y = 0 - nava.r;
-    
+    //pentru a nu iesi din ecran:
+    if (nava.x < 0 - nava.r)
+        nava.x = canvas.width + nava.r;
+    else
+        if (nava.x > canvas.width + nava.r)
+            nava.x = 0 - nava.r;
+    if (nava.y < 0 - nava.r)
+        nava.y = canvas.height + nava.r;
+    else
+        if (nava.y > canvas.height + nava.r)
+            nava.y = 0 - nava.r;
+
     // ---------------------------------------------------------------------- NAVA
 
-    
+
     // ---------------------------------------------------------------------- ASTEROID
 
     //desenez asterorizii
@@ -388,7 +510,7 @@ function update() {
 
         // ---------------------------------------------------------------------- ACOLIZIUNI
 
-       
+
     }
     //mut asteroidul
     for (var i = 0; i < asteroizi.length; i++) {
@@ -418,12 +540,14 @@ function update() {
 
 
     // ---------------------------------------------------------------------- COLIZIUNI
-    if(!explozieNava){
-    //verific daca exista coliziuni cu asteroizii
-    if(nava.nrLicarit==0){
-        for (var i = 0; i < asteroizi.length; i++) {
-            if (distantaDintrePuncte(nava.x, nava.y, asteroizi[i].x, asteroizi[i].y) < nava.r + asteroizi[i].r)
-                explodeazaNava();
+    if (!explozieNava) {
+        //verific daca exista coliziuni cu asteroizii
+        if (nava.nrLicarit == 0) {
+            for (var i = 0; i < asteroizi.length; i++) {
+                if (distantaDintrePuncte(nava.x, nava.y, asteroizi[i].x, asteroizi[i].y) < nava.r + asteroizi[i].r){
+                    explodeazaNava();
+                    distrugeAsteroid(i);}
+               
             }
         }
     }
